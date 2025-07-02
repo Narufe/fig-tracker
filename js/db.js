87e7853a65a1351b6e1371efaa9f46cb7467a6a3
Rafe Taya <rafe.otaya@gmail.com>
2025-07-02 03:34:58 +0300

    Push index.html and latest changes

---
+++ js/db.js
@@ -0,0 +1,8 @@
+console.log('Loaded db.js');
+
+const SUPABASE_URL = 'https://ygcjrxqtzpxowvlhhqqb.supabase.co';
+
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnY2pyeHF0enB4b3d2bGhocXFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzNzAxNTYsImV4cCI6MjA2Njk0NjE1Nn0.C9aLiYQG95Pgu1qG6vbmjspBuxgGCRrwJvO8w2z1HgQ' 
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_PUBLIC_KEY);
+
